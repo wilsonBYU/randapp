b5be5711edd4a290aec9e9d78eaf27d84404d3b2
wilsonBYU <wilsonomar@byui.edu>
2024-03-29 18:03:12 -0600

    fixed vite.config build, missing comma

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-  base: "/randapp/"
+  base: "/randapp/",
   build: {
     outDir: "../dist",
     rollupOptions: {
